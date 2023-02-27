a985f31d2a33e517a65181a95cdfa78973699a75
babyoilrig <catrin.mentzoni@yahoo.com>
2023-02-27 16:43:32 +0000

    Changed code back to original

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const showFactsButton = document.getElementById("show-facts-button");
 
-showFactsButton.addEventListener("click", showFacts());
+// showFactsButton.addEventListener("click", showFacts());
 
 function showFacts() {
+    console.log("I have been clicked!");
     var factsContainer = document.getElementById("factsContainer");
     if (factsContainer.style.display === "none") {
       factsContainer.style.display = "block";
@@ -10,3 +11,6 @@ function showFacts() {
       factsContainer.style.display = "none";
     }
   }
+
+  //This isn't working atm - need to append child??
+  //Also on page the load the image is really big for some reason
